21d49b3948f7aeddf8de6609f6f64a74c0a3e48c
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-10-11 22:39:31 +0530

    Added Employee Wage UC4.

--- com/bridgelabz/EmployeeWage.java
+++ com/bridgelabz/EmployeeWage.java
@@ -10,19 +10,22 @@ package com.bridgelabz;
 
         public static void main(String[] args) {
 
-            int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
+            int employeeCheck = (int) Math.floor(Math.random() * 3) ;
             System.out.println(employeeCheck);
             int dailyWage = 0;
-            if (employeeCheck == 2) {
+           switch (employeeCheck){
+               case 2:
                 System.out.println("Employee is present");
                     dailyWage = EMP_WAGE_PER_HOUR * DAILY_TIME_HOUR;
                     dailyWage = EMP_WAGE_PER_HOUR * FULL_TIME_HOUR;
                     System.out.println("Daily wage is " + dailyWage);
-                } else if (employeeCheck == 1) {
+               break;
+               case 1:
                     System.out.println("Employee is present");
                     dailyWage = EMP_WAGE_PER_HOUR * PART_TIME_HOUR;
                     System.out.println("Daily wage is " + dailyWage);
-                } else {
+                 break;
+               default:
                     System.out.println("Employee is absent");
                     System.out.println("Daily wage is " + dailyWage);
                 }
