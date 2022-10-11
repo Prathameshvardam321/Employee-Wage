8eb8fe2340ae6282b34069f8720958e057b5f961
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-10-11 22:53:06 +0530

    Added Employee Wage UC5.

--- com/bridgelabz/EmployeeWage.java
+++ com/bridgelabz/EmployeeWage.java
@@ -9,27 +9,34 @@ package com.bridgelabz;
         static int PART_TIME_HOUR = 4;
 
         public static void main(String[] args) {
-
-            int employeeCheck = (int) Math.floor(Math.random() * 3) ;
-            System.out.println(employeeCheck);
-            int dailyWage = 0;
-           switch (employeeCheck){
-               case 2:
-                System.out.println("Employee is present");
-                    dailyWage = EMP_WAGE_PER_HOUR * DAILY_TIME_HOUR;
-                    dailyWage = EMP_WAGE_PER_HOUR * FULL_TIME_HOUR;
-                    System.out.println("Daily wage is " + dailyWage);
-               break;
-               case 1:
-                    System.out.println("Employee is present");
-                    dailyWage = EMP_WAGE_PER_HOUR * PART_TIME_HOUR;
-                    System.out.println("Daily wage is " + dailyWage);
-                 break;
-               default:
-                    System.out.println("Employee is absent");
-                    System.out.println("Daily wage is " + dailyWage);
+            int days = 20;
+            int totalWage = 0;
+            for (int i = 1; i <= days; i++) {
+                int employeeCheck = (int) Math.floor(Math.random() * 3);
+                System.out.println("For day "+i);
+                System.out.println(employeeCheck);
+                int dailyWage = 0;
+                switch (employeeCheck) {
+                    case 2:
+                        System.out.println("Employee is present");
+                        dailyWage = EMP_WAGE_PER_HOUR * DAILY_TIME_HOUR;
+                        dailyWage = EMP_WAGE_PER_HOUR * FULL_TIME_HOUR;
+                        totalWage=totalWage+dailyWage;
+                        System.out.println("Daily wage is " + dailyWage);
+                        break;
+                    case 1:
+                        System.out.println("Employee is present");
+                        dailyWage = EMP_WAGE_PER_HOUR * PART_TIME_HOUR;
+                        System.out.println("Daily wage is " + dailyWage);
+                        totalWage=totalWage+dailyWage;
+                        break;
+                    default:
+                        System.out.println("Employee is absent");
+                        System.out.println("Daily wage is " + dailyWage);
                 }
             }
+            System.out.println("Total wage is : "+totalWage);
+        }
         }
 
 
