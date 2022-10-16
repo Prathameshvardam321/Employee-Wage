c8c330c64ea48bd011ee52515c7d877d941b82e1
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-10-16 12:09:30 +0530

    Added Employee Wage UC8.

--- com/bridgelabz/EmployeeWage.java
+++ com/bridgelabz/EmployeeWage.java
@@ -2,50 +2,43 @@ package com.bridgelabz;
 
 
     public class EmployeeWage {
-
-        static final int DAILY_TIME_HOUR = 8;
-        static final int EMP_WAGE_PER_HOUR = 20;
-        static int FULL_TIME_HOUR = 8;
-        static int PART_TIME_HOUR = 4;
-        public static void computeEmployeeWage(){
-            int totalDays = 20;
-            int totalWage = 0;
-            int totalHours = 0;
+        public static void employeeWageBuilder(String companyName,int fullTimeHour,int wagePerHour,int totalDays){
             int day = 1;
-
+            int partTimeHour=1/2*fullTimeHour;
+            int totalWage=0;
+            int totalHours=0;
             while (totalHours <= 100 && day <= totalDays) {
-                int employeeCheck = (int) Math.floor(Math.random() * 3);
-                System.out.println(employeeCheck);
-                int dailyWage = 0;
-                switch (employeeCheck) {
+                    int employeeCheck = (int) Math.floor(Math.random() * 3);
+                    System.out.println(employeeCheck);
+                    int dailyWage = 0;
+                    switch (employeeCheck) {
                     case 2:
-                        System.out.println("Employee is Full time.");
-                        dailyWage = EMP_WAGE_PER_HOUR * FULL_TIME_HOUR;
-                        totalHours+=FULL_TIME_HOUR;
-                        break;
+                    System.out.println("Employee is Full time.");
+                  dailyWage = wagePerHour*fullTimeHour;
+                    totalHours+=fullTimeHour;
+                    break;
                     case 1:
-                        System.out.println("Employee is Part time.");
-                        dailyWage = EMP_WAGE_PER_HOUR * PART_TIME_HOUR;
-                        totalHours+=PART_TIME_HOUR;
-                        break;
-                    default:
-                        System.out.println("Employee is absent");
-
-
-                }
+                    System.out.println("Employee is Part time.");
+                   dailyWage = wagePerHour*partTimeHour;
+                    totalHours+=partTimeHour;
+                    break;
+                   default:
+        System.out.println("Employee is absent");
+                    }
+        totalWage = totalWage + dailyWage;
+        System.out.println("Daily wage is for "+ companyName+" is "+ dailyWage+ " for day "+day);
                 day++;
-                totalWage = totalWage + dailyWage;
-                System.out.println("Daily wage is " + dailyWage+ " for day "+day);
-            }
-            System.out.println("Total working hours : "+totalHours);
-            System.out.println("Total wage is : " + totalWage);
+        }
+        System.out.println("Total working hours for "+companyName+" is : "+totalHours);
+        System.out.println("Total wage for "+companyName+ " is : " + totalWage);
+        System.out.println("-------------------------------------------------------------");
+
         }
 
         public static void main(String[] args) {
-           computeEmployeeWage();
+          employeeWageBuilder("Mahendra",12,20,22);
+          employeeWageBuilder("Reliance",14,24,20);
         }
         }
-
-
 
 
