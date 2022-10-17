2810051db25bc100f8f609f9c238a111f44883db
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-10-17 23:43:00 +0530

    Added Employee Wage UC9.

--- com/bridgelabz/EmployeeWage.java
+++ com/bridgelabz/EmployeeWage.java
@@ -1,44 +1,74 @@
 package com.bridgelabz;
 
 
-    public class EmployeeWage {
-        public static void employeeWageBuilder(String companyName,int fullTimeHour,int wagePerHour,int totalDays){
-            int day = 1;
-            int partTimeHour=1/2*fullTimeHour;
-            int totalWage=0;
-            int totalHours=0;
-            while (totalHours <= 100 && day <= totalDays) {
-                    int employeeCheck = (int) Math.floor(Math.random() * 3);
-                    System.out.println(employeeCheck);
-                    int dailyWage = 0;
-                    switch (employeeCheck) {
-                    case 2:
-                    System.out.println("Employee is Full time.");
-                  dailyWage = wagePerHour*fullTimeHour;
-                    totalHours+=fullTimeHour;
+public class EmployeeWage {
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
+    private int EMP_WAGE_PER_HOUR ;
+    private int FULL_TIME_HOUR ;
+    private int PART_TIME_HOUR ;
+    private int WORKING_DAYS_PER_MONTH ;
+
+    private int TOTAL_EMPLOYEE_WAGE ;
+
+
+
+    public EmployeeWage() {
+    }
+
+    public EmployeeWage(int EMP_WAGE_PER_HOUR, int FULL_TIME_HOUR, int PART_TIME_HOUR, int WORKING_DAYS_PER_MONTH) {
+        this.EMP_WAGE_PER_HOUR = EMP_WAGE_PER_HOUR;
+        this.FULL_TIME_HOUR = FULL_TIME_HOUR;
+        this.PART_TIME_HOUR = PART_TIME_HOUR;
+        this.WORKING_DAYS_PER_MONTH = WORKING_DAYS_PER_MONTH;
+    }
+
+    public void calculateEmployeeWage() {
+
+        int empHours = 0 ;
+        int day = 1;
+        int totalWage = 0;
+        while (day < WORKING_DAYS_PER_MONTH && empHours <= 100) {
+            int dailyWage = 0;
+            int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
+            System.out.println(employeeCheck);
+            switch (employeeCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is present");
+                    dailyWage = EMP_WAGE_PER_HOUR * FULL_TIME_HOUR;
+                    empHours += FULL_TIME_HOUR ;
+
                     break;
-                    case 1:
-                    System.out.println("Employee is Part time.");
-                   dailyWage = wagePerHour*partTimeHour;
-                    totalHours+=partTimeHour;
+                case IS_PART_TIME:
+                    System.out.println("Employee is present");
+                    dailyWage = EMP_WAGE_PER_HOUR * PART_TIME_HOUR;
+                    empHours += PART_TIME_HOUR ;
+
                     break;
-                   default:
-        System.out.println("Employee is absent");
-                    }
-        totalWage = totalWage + dailyWage;
-        System.out.println("Daily wage is for "+ companyName+" is "+ dailyWage+ " for day "+day);
-                day++;
+                default:
+                    System.out.println("Employee is absent");
+            }
+            day++;
+            totalWage += dailyWage ;
+            System.out.println("Total employee hours are " + empHours);
+            System.out.println("Total wage for " + day + " day is " + totalWage);
         }
-        System.out.println("Total working hours for "+companyName+" is : "+totalHours);
-        System.out.println("Total wage for "+companyName+ " is : " + totalWage);
-        System.out.println("-------------------------------------------------------------");
+        TOTAL_EMPLOYEE_WAGE = totalWage;
 
-        }
+    }
 
-        public static void main(String[] args) {
-          employeeWageBuilder("Mahendra",12,20,22);
-          employeeWageBuilder("Reliance",14,24,20);
-        }
-        }
+    @Override
+    public String toString() {
+        return "total employee wage = " + TOTAL_EMPLOYEE_WAGE ;
+    }
 
+    public static void main(String[] args) {
+        EmployeeWage perfios  = new EmployeeWage(35,8,5,23);
+        perfios.calculateEmployeeWage();
+        System.out.println("Perfios " + perfios.toString());
+        EmployeeWage reliance = new EmployeeWage(30,14,7,24);
+        reliance.calculateEmployeeWage();
+        System.out.println("Reliance " + reliance.toString());
 
+    }
+}
